53465839b4ee90648d0c1314d14f5a03695c4269
Jessica Guo <jessicaguo@email.arizona.edu>
2020-10-17 17:53:53 +0000

    Create Gs.R function and revise collating part of control script

--- R/Gs.R
+++ R/Gs.R
@@ -1,25 +1,41 @@
 # Function to estimate stomatal parameters with 'plantecophys'
-# Also produces estimates of A and gsw at standard conditions of PAR > 1200 umol/m^2/s and 390 ppm < CO2 > 410 ppm
+# Uses both ACi and AQ data at standard conditions of PAR > 1200 umol/m^2/s and 390 ppm < CO2 > 410 ppm
 
 Gs <- function(fileID){# input is ID column from the experiments dataframe
   
   # Required packages
   library(ggplot2)
   library(plantecophys)
+  library(dplyr)
 
   # Read in data
-  aci <- read.csv(paste0("cleaned_data/ACi/A_Ci_curves_", fileID, ".csv"))
-  aq <- read.csv(paste0("cleaned_data/AC/AQin_curves_", fileID, ".csv"))
-  
-  # Select standard conditions and combine
-  aci <- subset(aci, CO2_s >= 390 & CO2_s <= 410 & gsw > 0)
-  aq <- subset(aq, Qin >= 1200 & gsw > 0)
-  
-  df <- rbind.data.frame(aci, aq)
+  fileNames <- dir("cleaned_data/", pattern = as.character(fileID), recursive = T)
+  df <- data.frame()
+  for(i in 1:length(fileNames)){
+    # Read in csv
+    temp <- read.csv(paste0("cleaned_data/", fileNames[i]))
+    
+    # Select relevant columns
+    temp2 <- subset(temp, select = c(species, rep, obs, time, date, hhmmss, 
+                                     CO2_s, Qin, A, gsw, VPDleaf, RHcham, Ca))
+    
+    # Filter by standard conditions
+    if(substr(fileNames[i], 1, 3) == "ACi"){
+      temp3 <- subset(temp2, CO2_s >= 390 & CO2_s <= 410 & gsw > 0)
+    } else if(substr(fileNames[i], 1, 2) == "AQ"){
+      temp3 <- subset(temp2, Qin >= 1200 & gsw > 0)
+    }
+    
+    # Remove outliers
+    temp3$A <- ifelse(temp3$gsw >0.15 & temp3$A>14 & temp3$A < 23, NA, temp3$A)
+    
+    # Combine
+    df <- rbind.data.frame(df, temp3)
+  }
   
   # Use 'plantecophys' to estimate stomatal parameters for Ball-Berry and Medlyn models
   #First: fit Medlyn et al. (2011) equation
-  gsfit  <- fitBB(df, varnames= list(ALEAF="A", GS= "gsw", VPD="VPDleaf", Ca="CO2_s", RH="RHcham"), 
+  gsfit  <- fitBB(df, varnames= list(ALEAF="A", GS= "gsw", VPD="VPDleaf", Ca="Ca", RH="RHcham"), 
                   gsmodel=c("BBOpti"), fitg0 = T) 
   g1M     <- summary(gsfit$fit)$parameters[1]				# save g1 from fitted model
   g0M     <- summary(gsfit$fit)$parameters[2]	      # save g0 from fitted model
@@ -44,26 +60,32 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
                     Value = c(g0M, g1M, g0BB, g1BB),
                     SE = c(g0M_se, g1M_se, g0BB_se, g1BB_se),
                     SD = rep(NA, 4),
-                    Date.run = rep(as.Date(Sys.time()), 6))
+                    Date.run = rep(as.Date(Sys.time()), 4))
   write.csv(out, file = paste0(loc, fileID, "_parameters.csv"), row.names = F)
   
   
   # Visualize across 2 replicates
   # create dataframe for plotting
   DF <- data.frame(gsw = rep(df$gsw, 2),
-                   x = c(df$A*df$RHcham/df$Ca,
-                         df$A/(df$Ca*sqrt(df$VPDleaf))),
+                   x = c(df$A*df$RHcham/df$Ca/100, #divide RH/100
+                         df$A/(df$CO2_s*sqrt(df$VPDleaf))),
                    type = c(rep("BB", nrow(df)), rep("M", nrow(df))))
+  params <- data.frame(type = c("BB", "M"),
+                       slope = c(g1BB, g1M),
+                       int = c(g0BB, g0M))
   
-  fig_stomatal <- ggplot(DF, aes(x=x, y=gsw))+
-    geom_point(aes(col = rep))+
-    facet_wrap(~type, ncol=2)+
+  # Medlyn model is not strictly linear, therefore no line shows up
+  
+  fig_stomatal <- ggplot()+
+    geom_point(data = DF, aes(x = x, y = gsw))+
+    geom_abline(data = params, aes(slope = slope, intercept = int))+
+    facet_wrap(~type, ncol=2, scales = "free")+
+    scale_y_continuous(expression(paste(g[sw])))+
     theme_bw()
   
-  pdf(paste0(loc, "diagnostic/", fileID, "_stomatal_curves.pdf"))
+  pdf(paste0(loc, "diagnostic/", fileID, "_stomatal_curves.pdf"), width = 6, height = 3)
   print(fig_stomatal)
   dev.off()
-
   
 }
   
--- parameter_estimation.R
+++ parameter_estimation.R
@@ -10,7 +10,8 @@ source("R/Gs.R")
 # Read in list of experiments
 # Metadata describing Date (unique), Genotype, and Treatment
 # A_Ci and A_Qin data are available
-expDF <- read.csv("cleaned_data/experiments.csv")
+expDF <- read.csv("cleaned_data/experiments.csv",
+                  colClasses = c("character", "numeric", rep("character",2), rep("numeric",2)))
 
 # Check date of previous run
 params <- read.csv("outputs/parameters_data.csv")
@@ -18,7 +19,8 @@ max(params$Date)
 
 # Step 1: Run C4_Collatz.R model on ACi data
 aci_ID <- expDF$ID[which(expDF$A_Ci == 1 # A_Ci data is present
-                         expDF$Date > max(params$Date))] # experiment date is more recent than last estimation run date
+                         & expDF$Date > max(params$Date)# experiment date is more recent than last estimation run date
+                         )] 
 for(i in 1:length(aci_ID)){
   C4_Collatz(fileID = aci_ID[i])
   print(paste0(aci_ID[i], " completed"))
@@ -26,27 +28,45 @@ for(i in 1:length(aci_ID)){
 
 
 # Step 2: Run LRC.R routine on AQ data
-aq_ID <- expDF$ID[which(expDF$A_Qin == 1)]
+aq_ID <- expDF$ID[which(expDF$A_Qin == 1 # A_Qin data is present
+                        & expDF$Date > max(params$Date)# experiment date is more recent than last estimation run date
+                        )]
 for(i in 1:length(aq_ID)){
   LRC(fileID = aq_ID[i])
   print(paste0(aq_ID[i], " completed"))
 }
 
 # Step 3: Run the Gs.R routine on subest of both ACi and AQ data
+ID <- expDF$ID[which(expDF$Date > max(params$Date)# experiment date is more recent than last estimation run date
+              )]
+for(i in 1:length(ID)){
+  Gs(fileID = ID[i])
+  print(paste0(ID[i], " completed"))
+}
 
 
 # Step 4: Collate the parameters
 aci <- list.files("outputs/ACi", pattern = "csv")
 params <- data.frame()
 for(i in 1:length(aci)){
-  temp <- read.csv(paste0("outputs/ACi/", aci[i]))
+  temp <- read.csv(paste0("outputs/ACi/", aci[i]),
+                   colClasses = c(rep("character", 3), rep("numeric",3), "character"))
   params <- rbind.data.frame(params, temp)
 }
 aq <- list.files("outputs/AQ", pattern = "csv")
 for(i in 1:length(aq)){
-  temp <- read.csv(paste0("outputs/AQ/", aq[i]))
+  temp <- read.csv(paste0("outputs/AQ/", aq[i]),
+                   colClasses = c(rep("character", 3), rep("numeric",3), "character"))
+  params <- rbind.data.frame(params, temp)
+}
+
+st <- list.files("outputs/stomatal", pattern = "csv")
+for(i in 1:length(st)){
+  temp <- read.csv(paste0("outputs/stomatal/", st[i]),
+                   colClasses = c(rep("character", 3), rep("numeric",3), "character"))
   params <- rbind.data.frame(params, temp)
 }
 
 params2 <- left_join(params, expDF, by = "ID")
+
 write.csv(params2, file = "outputs/parameters_data.csv", row.names = F)
