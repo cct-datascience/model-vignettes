6edf3240805b35a2420879cc6a3fee4c37b300e4
David LeBauer <dlebauer@email.arizona.edu>
2020-12-09 18:52:00 +0000

    added example workflow and xml for ed2

---
+++ ED2/ed2workflow.R
@@ -0,0 +1,38 @@
+# ----------------------------------------------------------------------
+# Load required libraries
+# ----------------------------------------------------------------------
+library(PEcAn.all)
+library(PEcAn.utils)
+library(RCurl)
+
+# ----------------------------------------------------------------------
+# PEcAn Workflow
+# ----------------------------------------------------------------------
+# Open, read in, and modify settings file for PEcAn run.
+settings <- PEcAn.settings::read.settings("pecan.xml") 
+settings <- PEcAn.settings::prepare.settings(settings, force = FALSE)
+PEcAn.settings::write.settings(settings, outputfile = "pecan.CHECKED.xml")
+settings <- PEcAn.workflow::do_conversions(settings)
+
+# Query the trait database for data and priors
+settings <- PEcAn.workflow::runModule.get.trait.data(settings)
+
+# Run the PEcAn meta.analysis
+PEcAn.MA::runModule.run.meta.analysis(settings)
+
+# Write model specific configs
+settings <- PEcAn.workflow::runModule.run.write.configs(settings)
+
+# Start ecosystem model runs
+PEcAn.remote::runModule.start.model.runs(settings, stop.on.error = FALSE)
+
+# Get results of model runs
+runModule.get.results(settings)
+
+# Run ensemble analysis on model output.
+runModule.run.ensemble.analysis(settings, TRUE)
+
+# Run sensitivity analysis and variance decomposition on model output
+runModule.run.sensitivity.analysis(settings)
+
+print("---------- PEcAn Workflow Complete ----------")
