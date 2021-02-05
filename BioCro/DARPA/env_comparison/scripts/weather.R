62093a5e5d9f86fcab7d05e821c805f684f77238
Jessica Guo <jessicaguo@email.arizona.edu>
2021-02-05 22:44:19 +0000

    Adjust weather to be date-corrected, with 7 days of germination under chamber conditions first, limit to 100 days

--- BioCro/DARPA/env_comparison/scripts/weather.R
+++ BioCro/DARPA/env_comparison/scripts/weather.R
@@ -9,22 +9,65 @@ library(ggplot2)
 # Outdoor data is weather station data + irrigation
 
 # Generate chamber (31_22_450) weather data 
-ch_weather <- data.frame(year = rep(2020, 365*24),
-                         doy = rep(1:365, each = 24),
-                         hour = rep(seq(0, 23), 365), 
-                         SolarR = rep(c(rep(0, each = 8), rep(430, each = 12), rep(0, each = 4)), times = 365),
-                         Temp = rep(c(rep(22, each = 8), rep(31, each = 12), rep(22, each = 4)), times = 365),
-                         RH = rep(55.5 / 100,  times = 365*24), 
-                         WS = rep(0, times = 365*24), 
-                         precip = rep(c(0.000462963, rep(0, 23)), 365))
+ch_weather <- data.frame(year = rep(2020, 100*24),
+                         doy = rep(1:100, each = 24),
+                         hour = rep(seq(0, 23), 100), 
+                         SolarR = rep(c(rep(0, each = 8), rep(430, each = 12), rep(0, each = 4)), times = 100),
+                         Temp = rep(c(rep(22, each = 8), rep(31, each = 12), rep(22, each = 4)), times = 100),
+                         RH = rep(55.5 / 100,  times = 100*24), 
+                         WS = rep(0, times = 100*24), 
+                         precip = rep(c(0.000462963, rep(0, 23)), 100))
 
-# Import actual data
+# Greenhouse and outdoor data started with 7 days in the growth chamber
+# Retrieve GH and Field sowing and transplant dates
+load("~/sentinel-detection/data/cleaned_data/biomass/greenhouse_outdoor_biomass.Rdata")
+exp_dates <- greenhouse_outdoor_biomass %>%
+  filter(exp_site == "GH"  & exp_number == 1 |
+           exp_site == "Field"  & exp_number == 2) %>%
+  group_by(exp_site) %>%
+  summarize(sowing = unique(sowing_date),
+            transplant = unique(transplant_date)) %>%
+  mutate(chamber_dur = difftime(transplant, sowing, units = "days"))
+
+
+# Refer to weather files in sentinel-detection repo
 data_path <- "~/sentinel-detection/data/raw_data/biomass/field_and_greenhouse_experiments.xlsx"
 sheets_names <- excel_sheets(data_path)
 
+# Outdoor weather data on top of B greenhouses at Danforth
+gh_out <- read_excel(data_path, sheets_names[15], range = "K1:N8784") %>% 
+  rename(dt = "Date/Time",
+         temp_C = "WS1:Outdoor Temp.(°C)",
+         light_w_m2 = "WS1:Outdoor Light(W/m²)",
+         wind_km_h = "WS1:Wind Speed(km/h)") %>%
+  mutate(SolarR = light_w_m2/2.35e5*1e6)
+
+# Indoor temp and relative humidity
+gh_in <- read_excel(data_path, sheets_names[15], range = "A1:I8784") %>% 
+  select(1,2,7) %>%
+  rename(dt = "Date/Time",
+         temp_C = "GH2B_Climate_Temperature_°C",
+         RH = "GH2B_Climate_Humidity_%Rh")
+
+# Greenhouse, with irrigation 3 mm/day divided into 10 am and 3 pm watering
+# Use only 93 days starting from transplant date
+ind <- which(gh_in$dt == exp_dates$transplant[exp_dates$exp_site == "GH"])
+gh_comb <- data.frame(year = rep(2020, 93*24),
+                         doy = rep(8:100, each = 24),
+                         hour = rep(seq(0, 23), 93), 
+                         SolarR = gh_out$SolarR[ind:(ind + 93*24 - 1)],
+                         Temp = gh_in$temp_C[ind:(ind + 93*24 - 1)],
+                         RH = gh_in$RH[ind:(ind + 93*24 - 1)]/100,
+                         WS = rep(0, times = 93*24), 
+                         precip = rep(c(rep(0, 10), 1.5, rep(0, 4), 1.5, rep(0, 8)), 93))
+
+# Add 7 days from ch_weather
+gh_weather <- rbind.data.frame(ch_weather[1:(7*24), ], gh_comb)
+
+
 # Outdoor weather data near Cella Farms, plus watering at 9 am 5 mm/day every day except Sundays
 # Source: Moscow Mills, Missouri Historical Agricultural Weather Database
-out_weather <- read_excel(data_path, sheets_names[13], range = cell_cols("A:J")) %>%
+out_cella<- read_excel(data_path, sheets_names[13], range = cell_cols("A:J")) %>%
   mutate(mon = as.character(as.Date(paste0(YEAR, "-", MONTH, "-01"))),
          dt = as.POSIXct((`HOUR  AVG`+6)*60*60 + (DAY-1)*24*60*60, origin = mon, tz = "America/Chicago")) %>%
   rename(year = YEAR,
@@ -45,52 +88,20 @@ out_weather <- read_excel(data_path, sheets_names[13], range = cell_cols("A:J"))
   select(year, doy, hour, SolarR, Temp, RH, WS, precip) %>%
   filter(doy != 366)
 
-# Outdoor weather data on top of B greenhouses at Danforth
-gh_out <- read_excel(data_path, sheets_names[15], range = "K1:N8784") %>% 
-  rename(dt = "Date/Time",
-         temp_C = "WS1:Outdoor Temp.(°C)",
-         light_w_m2 = "WS1:Outdoor Light(W/m²)",
-         wind_km_h = "WS1:Wind Speed(km/h)") %>%
-  mutate(SolarR = light_w_m2/2.35e5*1e6)
-  
-# Generate greenhouse (1st experiment) weather data from measured data
-# Greenhouse plants watered 3mm/day, 1.5 mm at 10 am and 3 pm
-# gh_light <- read_excel(data_path, sheets_names[23], range = "F1:J17") %>%
-#   rename(dt = `date/time`,
-#          light_1 = `manual_measure_light_intensity_umol/m2/s...4`,
-#          light_2 = `manual_measure_light_intensity_umol/m2/s...5`) %>%
-#   tidyr::pivot_longer(cols = c("light_1", "light_2"), names_to = "rep", values_to = "light")
-# 
-# ggplot(gh_light, aes(x = dt))+
-#   geom_point(aes(y = light, color = treatment)) +
-#   theme_bw()
+# Use only 93 days from transplanting
+ind <- which(gh_in$dt == exp_dates$transplant[exp_dates$exp_site == "Field"])
+out_comb <- data.frame(year = rep(2020, 93*24),
+                      doy = rep(8:100, each = 24),
+                      hour = rep(seq(0, 23), 93), 
+                      SolarR = out_cella$SolarR[ind:(ind + 93*24 - 1)],
+                      Temp = out_cella$Temp[ind:(ind + 93*24 - 1)],
+                      RH = out_cella$RH[ind:(ind + 93*24 - 1)]/100,
+                      WS = out_cella$WS[ind:(ind + 93*24 - 1)], 
+                      precip = out_cella$precip[ind:(ind + 93*24 - 1)])
 
-# Match to outdoor data
-# gh_match <- gh_light %>%
-#   mutate(dt_round = round(dt, "hour")) %>%
-#   left_join(sc[,c("dt", "SolarR")], by = c("dt_round" = "dt"))
-# 
-# ggplot(gh_match, aes(x = SolarR))+
-#   geom_abline(slope = 1, intercept = 0) +
-#   geom_point(aes(y = light, color = as.factor(dt))) +
-#   theme_bw()
+# Add 7 days from ch_weather
+out_weather <- rbind.data.frame(ch_weather[1:(7*24), ], out_comb)
 
-
-gh_in <- read_excel(data_path, sheets_names[15], range = "A1:I8784") %>% 
-  select(1,2,7) %>%
-  rename(dt = "Date/Time",
-         temp_C = "GH2B_Climate_Temperature_°C",
-         RH = "GH2B_Climate_Humidity_%Rh")
-
-# Greenhouse, with irrigation 3 mm/day divided into 10 am and 3 pm watering
-gh_weather <- data.frame(year = rep(2020, 365*24),
-                         doy = rep(1:365, each = 24),
-                         hour = rep(seq(0, 23), 365), 
-                         SolarR = gh_out$SolarR[1:(365*24)],
-                         Temp = gh_in$temp_C[1:(365*24)],
-                         RH = gh_in$RH[1:(365*24)]/100,
-                         WS = rep(0, times = 365*24), 
-                         precip = rep(c(rep(0, 10), 1.5, rep(0, 4), 1.5, rep(0, 8)), 365))
 
 # Write out weather files as csv
 write.csv(ch_weather, 
