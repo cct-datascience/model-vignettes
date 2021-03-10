f577b55ab0a8215972fc2b8d7ab15716a72f4840
Jessica Guo <jessicaguo@email.arizona.edu>
2021-03-10 21:16:00 +0000

    Fix D0 in BBL to 1.6, ED2 standard for C4 grass

--- parameters_processing/R/Gs2.R
+++ parameters_processing/R/Gs2.R
@@ -11,7 +11,7 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
   require(dplyr)
 
   # Read in data
-  fileNames <- dir("../../sentinel-detection/data/cleaned_data/", pattern = as.character(fileID), recursive = T)
+  fileNames <- dir("~/sentinel-detection/data/cleaned_data/", pattern = as.character(fileID), recursive = T)
   
   # Remove if Rd files present (currently, Rd file does not include raw data and cannot be used for stomatal parameter estimation)
   ind <- which(substr(fileNames, 1, 2) == "Rd")
@@ -22,7 +22,7 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
   df <- data.frame()
   for(i in 1:length(fileNames)){
     # Read in csv
-    temp <- read.csv(paste0("../../sentinel-detection/data/cleaned_data/", fileNames[i]))
+    temp <- read.csv(paste0("~/sentinel-detection/data/cleaned_data/", fileNames[i]))
     
     # Select relevant columns
     temp2 <- subset(temp, select = c(species, rep, obs, time, date, hhmmss, 
@@ -36,8 +36,8 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
     
     # Filter AQ data for Qin >  LCPT (estimated)
     if(substring(fileNames[i], 1, 2) == "AQ"){
-      lparams <- read.csv(paste0("../../sentinel-detection/data/derived_data/AQ/", 
-                                 dir("../../sentinel-detection/data/derived_data/AQ/", 
+      lparams <- read.csv(paste0("~/sentinel-detection/data/derived_data/AQ/", 
+                                 dir("~/sentinel-detection/data/derived_data/AQ/", 
                                      pattern = as.character(fileID))))
       temp3 <- rbind.data.frame(subset(temp2, rep == "plant_1" & Qin >= lparams$Value[lparams$trait == "LCPT" & lparams$rep == "plant_1"]),
                                 subset(temp2, rep == "plant_2" & Qin >= lparams$Value[lparams$trait == "LCPT" & lparams$rep == "plant_2"]),
@@ -78,7 +78,7 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
     
     # Third: fit the Ball-Berry-Leuning (1995) model    
     gsfit3  <- fitBB(dflist[[i]], varnames = list(ALEAF = "A", GS = "gsw", VPD = "VPDleaf", Ca ="Ca", RH ="RHcham"), 
-                     gsmodel = c("BBLeuning"), fitg0 = TRUE, D0 = 0.35) 
+                     gsmodel = c("BBLeuning"), fitg0 = TRUE, D0 = 1.6) 
     g1L     <- summary(gsfit3$fit)$parameters[1]				
     g0L     <- summary(gsfit3$fit)$parameters[2]	
     g1L_se  <- summary(gsfit3$fit)$parameters[1,2] 
@@ -98,10 +98,10 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
   }
   
   # Location of output files
-  if(dir.exists("../../sentinel-detection/data/derived_data/stomatal/diagnostic") == F){
-    dir.create("../../sentinel-detection/data/derived_data/stomatal/diagnostic", recursive = TRUE)
+  if(dir.exists("~/sentinel-detection/data/derived_data/stomatal/diagnostic") == F){
+    dir.create("~/sentinel-detection/data/derived_data/stomatal/diagnostic", recursive = TRUE)
   }
-  loc <- paste0("../../sentinel-detection/data/derived_data/stomatal/")
+  loc <- paste0("~/sentinel-detection/data/derived_data/stomatal/")
   write.csv(out, file = paste0(loc, fileID, "_parameters.csv"), row.names = F)
   
   # Plotting 
@@ -111,7 +111,7 @@ Gs <- function(fileID){# input is ID column from the experiments dataframe
                    gsw = rep(df$gsw, 3),
                    x = c(df$A*df$RHcham/df$Ca/100, #divide RH/100
                          df$A/(df$CO2_s*sqrt(df$VPDleaf)),
-                         df$A/df$Ca/(1+df$VPDleaf/0.35)),
+                         df$A/df$Ca/(1+df$VPDleaf/1.6)),
                    type = c(rep("BB", nrow(df)), rep("M", nrow(df)), rep("BBL", nrow(df))))
   params <- data.frame(type = rep(c("M", "BB", "BBL"), 3),
                        rep = rep(names(dflist), each = 3),
