ec23c3b1a7e6f1daf67b5f340632318a030868c2
Jessica Guo <jessicaguo@email.arizona.edu>
2021-03-29 18:08:56 +0000

    Add AGB to ts plots

--- BioCro/DARPA/temp_comparison/scripts/plot_ts.R
+++ BioCro/DARPA/temp_comparison/scripts/plot_ts.R
@@ -17,13 +17,13 @@ biomass_ts <- c()
 trans_ts <- c()
 for (trt in treatments) {
   data_in <- read.csv(paste0("/data/output/pecan_runs/temp_comp_results/", trt, 
-                        "/ensemble_ts_summary_TotLivBiom.csv")) %>%
+                             "/ensemble_ts_summary_AGB.csv")) %>%
     mutate(treatment = trt) %>%
     relocate(treatment)
   biomass_ts <- rbind(biomass_ts, data_in)
   
   data_in <- read.csv(paste0("/data/output/pecan_runs/temp_comp_results/", trt, 
-                         "/ensemble_ts_summary_TVeg.csv")) %>%
+                             "/ensemble_ts_summary_TVeg.csv")) %>%
     mutate(treatment = trt) %>%
     relocate(treatment)
   trans_ts <- rbind(trans_ts, data_in)
@@ -46,21 +46,23 @@ biomass_valid <- chamber_biomass %>%
          stem_DW_g = ud.convert(stem_DW_mg, "mg", "g"),
          leaf_DW_g = ud.convert(leaf_DW_mg, "mg", "g"),
          panicle_DW_g = ud.convert(panicle_DW_mg, "mg", "g")) %>%
+  # select closest dates to validation data, which were sown 2019-01-03 in GCH158
+  filter(sowing_date >= as.POSIXct("2018-11-01") & sowing_date <= as.POSIXct("2019-03-01")) %>%
   dplyr::select(location, plant_id, sowing_date, transplant_date, harvest_date,
-         stem_DW_g, leaf_DW_g, panicle_DW_g) %>%
+                stem_DW_g, leaf_DW_g, panicle_DW_g) %>%
   rename(treatment = location) %>%
-  mutate(total_biomass_kg_m2 = ud.convert((stem_DW_g + leaf_DW_g + panicle_DW_g)/103,
-                                          "g/cm2", "kg/m2"),
-         day = difftime(harvest_date, sowing_date, units = "days"))
+  mutate(agb_kg_m2 = ud.convert((stem_DW_g + leaf_DW_g + panicle_DW_g)/103,
+                                "g/cm2", "kg/m2"),
+         day = difftime(harvest_date, sowing_date, units = "days")) 
 
 # Plot measured biomass against biomass estimates
 fig_biomass_ts <- ggplot() +
   geom_line(data = biomass_ts, aes(day, y = median, color = treatment)) +
-  geom_ribbon(data = biomass_ts, aes(day, ymin = lcl_95, ymax = ucl_95, fill = treatment), 
+  geom_ribbon(data = biomass_ts, aes(day, ymin = lcl_50, ymax = ucl_50, fill = treatment), 
               alpha = 0.25) +
-  geom_point(data = biomass_valid, aes(day, y = total_biomass_kg_m2, color = treatment)) +
-  scale_x_continuous("Day of Experiment") + 
-  scale_y_continuous(expression(paste("Total Biomass (kg ",  m^-2, ")"))) +
+  geom_point(data = biomass_valid, aes(day, y = agb_kg_m2, color = treatment)) +
+  scale_x_continuous("Day of experiment") + 
+  scale_y_continuous(expression(paste("Abovground biomass (kg ",  m^-2, ")"))) +
   theme_classic()
 
 jpeg(filename = "../plots/biomass_ts.jpg", height = 5, width = 7, units = "in", res = 600)
@@ -69,7 +71,7 @@ dev.off()
 
 fig_trans_ts <- ggplot() +
   geom_line(data = trans_ts, aes(day, y = median, color = treatment)) +
-  geom_ribbon(data = trans_ts, aes(day, ymin = lcl_95, ymax = ucl_95, fill = treatment), 
+  geom_ribbon(data = trans_ts, aes(day, ymin = lcl_50, ymax = ucl_50, fill = treatment), 
               alpha = 0.25) +
   scale_x_continuous("Day of Experiment") + 
   scale_y_continuous(expression(paste("Canopy Transpiration (kg ",  m^-2, " ", day^-1, ")"))) +
