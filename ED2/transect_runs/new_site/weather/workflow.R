5489f74e922c24837009c97713d7a74e8787b596
Eric Scott <ericrscott@email.arizona.edu>
2022-07-15 15:31:25 +0000

    changed host name in workflow to be compatible with login.oceleote...

--- ED2/transect_runs/new_site/weather/workflow.R
+++ ED2/transect_runs/new_site/weather/workflow.R
@@ -15,7 +15,13 @@ settings <- do_conversions(settings, overwrite.met = TRUE)
 # pull path name from settings and add trailing slash
 driver_path_local <- paste0(dirname(settings[["run"]][["inputs"]][["met"]][["path"]][["path1"]]), "/")
 driver_path_local
-driver_path_hpc <- file.path("puma:", "groups/dlebauer/ed2_results/inputs/julianp/sites", basename(driver_path_local))
+
+# Edit `host` to be whatever is in your .ssh/config
+host <- "login.ocelote.hpc.arizona.edu"
+# "puma" is now registered in BETY if you want to use it
+# host <- "puma"
+
+driver_path_hpc <- file.path(paste0(host, ":"), "groups/dlebauer/ed2_results/inputs/julianp/sites", basename(driver_path_local))
 driver_path_hpc
 system2("rsync", args = c(
   "-a", "-q", "--delete",
